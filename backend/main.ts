807f047e5602eace1a83d7121bdc6a5062141ee0
SebGillman <sebinred1@gmail.com>
2024-12-27 16:07:25 +0000

    feat: create hono backend

--- backend/main.ts
+++ backend/main.ts
@@ -1,8 +1,10 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { Hono } from "@hono";
+import type { Context } from "@hono";
+function main(): void {
+  const app = new Hono();
+  Deno.serve(app.fetch);
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  main();
 }
